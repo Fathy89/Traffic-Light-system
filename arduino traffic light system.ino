17d6470502575c5042392754feb86ac222e7d3d6
Fathy89 <138721463+Fathy89@users.noreply.github.com>
2023-07-07 20:52:04 +0300

    Add files via upload

---
+++ arduino traffic light system.ino
@@ -0,0 +1,28 @@
+// C++ code
+//
+int redled= 6; 
+int yellowled= 4; 
+int greenled= 2; 
+void setup()
+{
+  pinMode(redled,OUTPUT); 
+  pinMode(yellowled,OUTPUT);
+  pinMode(greenled,OUTPUT); 
+
+}
+
+void loop()
+{digitalWrite(redled,HIGH);
+ delay(700);
+ for(int i=0 ; i<3; i++)
+ {digitalWrite(yellowled,HIGH) ;
+   delay(500); 
+  digitalWrite(yellowled,LOW); 
+   delay(500); 
+ }
+  digitalWrite(redled,LOW); 
+ digitalWrite(greenled,HIGH); 
+ delay(1000); 
+  digitalWrite(greenled,LOW);
+
+}
